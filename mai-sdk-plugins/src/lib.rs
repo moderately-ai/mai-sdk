dd3938bd7a61e253f9f262e24a1d00ced0416faf
Thomas Santerre <thomas@santerre.xyz>
2024-06-12 15:55:34 -0400

    feat: add initial structure for the voice generation plugin

--- mai-sdk-plugins/src/lib.rs
+++ mai-sdk-plugins/src/lib.rs
@@ -4,5 +4,8 @@ pub mod text_generation;
 #[cfg(feature = "transcription")]
 pub mod transcription;
 
-#[cfg(feature = "web-scraping")]
+#[cfg(feature = "web_scraping")]
 pub mod web_scraping;
+
+#[cfg(feature = "voice_generation")]
+pub mod voice_generation;
---
+++ mai-sdk-plugins/src/voice_generation/mod.rs
@@ -0,0 +1,55 @@
+use anyhow::Result;
+use mai_sdk_core::task_queue::{Runnable, TaskId};
+use slog::Logger;
+
+pub struct VoiceGenerationPluginState {
+    pub logger: Logger,
+}
+
+impl VoiceGenerationPluginState {
+    pub fn new(logger: &Logger) -> Self {
+        Self {
+            logger: logger.clone(),
+        }
+    }
+}
+
+pub enum VoicePresets {
+    Default,
+}
+
+pub struct VoiceGenerationPluginTask {
+    pub id: TaskId,
+    pub text: String,
+    pub voice: VoicePresets,
+}
+
+impl VoiceGenerationPluginTask {
+    pub fn new(text: String, voice: VoicePresets) -> Self {
+        Self {
+            id: TaskId::new(),
+            text,
+            voice,
+        }
+    }
+}
+
+pub struct VoiceGenerationPluginTaskOutput {
+    pub audio: Vec<u8>,
+}
+
+impl Runnable<VoiceGenerationPluginTaskOutput, VoiceGenerationPluginState>
+    for VoiceGenerationPluginTask
+{
+    fn id(&self) -> TaskId {
+        self.id.clone()
+    }
+
+    async fn run(
+        &self,
+        state: VoiceGenerationPluginState,
+    ) -> Result<VoiceGenerationPluginTaskOutput> {
+        // TODO: i'd like to implement this with the bark model, will need to port to candle, 
+        Ok(VoiceGenerationPluginTaskOutput { audio: vec![] })
+    }
+}
